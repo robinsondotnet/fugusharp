cfac5382a1c5499bea85162a6a8637df948f5098
Kento <robinsondotnet@hotmail.com>
2019-08-31 18:13:04 -0500

    Rename stuff

--- src/fugusharp/Recipes.fs
+++ src/fugusharp/Recipes.fs
@@ -14,11 +14,11 @@ module Recipes =
         fun (next: HttpFunc) (ctx: HttpContext) ->
             let client = CreateDbDriver
             use session = client.Session()
-            let txHandler = fun (tx: Neo4j.Driver.V1.ITransaction) ->
+            let writeTxHandler = fun (tx: Neo4j.Driver.V1.ITransaction) ->
                 tx.Run "MATCH (r: Recipe) RETURN r.name" 
                 |> Seq.map (fun (rawResult) ->
                    {Name = rawResult.Item(0).ToString()})
         
-            let recipes = session.WriteTransaction txHandler
+            let recipes = session.WriteTransaction writeTxHandler
 
             json recipes next ctx
